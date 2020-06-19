aa0a32b88b612a44851b8c30a823d562044c4c5a
Emmanuel Puerto <emmanuel.puerto@sifive.com>
2020-06-19 16:48:13 +0200

    Add missing cast
    Signed-off-by: Emmanuel Puerto <emmanuel.puerto@sifive.com>

--- include/api/hardware/hca_macro.h
+++ include/api/hardware/hca_macro.h
@@ -58,11 +58,11 @@ static __inline__ void hca_setfield32(const metal_scl_t *const scl,
 }
 
 #define GET_32BITS(data, k)                                                    \
-    ((*(data + k + 3) << 24) + (*(data + k + 2) << 16) +                       \
-     (*(data + k + 1) << 8) + (*(data + k)))
+    (((uint32_t)*((uint8_t *)data + k + 3) << 24) + ((uint32_t)*((uint8_t *)data + k + 2) << 16) +                       \
+     ((uint32_t)*((uint8_t *)data + k + 1) << 8) + ((uint32_t)*((uint8_t *)data + k)))
 #define GET_64BITS(data, k)                                                    \
-    ((((uint64_t)GET_32BITS(data, (k + 4))) << 32) +                           \
-     (uint64_t)GET_32BITS(data, k))
+    ((((uint64_t)GET_32BITS((uint8_t *)data, (k + 4))) << 32) +                           \
+     (uint64_t)GET_32BITS((uint8_t *)data, k))
 
 /** @}*/
 
