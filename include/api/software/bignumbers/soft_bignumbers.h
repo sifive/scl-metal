fde8ccca2b5284bd4fc49a78df478c86368273ca
Pierre-Henry Moussay <pierre-henry.moussay@sifive.com>
2020-06-28 00:00:18 +0200

    Add big integer substraction
    Signed-off-by: Pierre-Henry Moussay <pierre-henry.moussay@sifive.com>

--- include/api/software/bignumbers/soft_bignumbers.h
+++ include/api/software/bignumbers/soft_bignumbers.h
@@ -66,9 +66,10 @@
  *
  * @param[out] array        bignumber to zeroise
  * @param[in] nb_64b_words  number of 64 bits words to zeroize
- * 
+ *
  */
-CRYPTO_FUNCTION void soft_bignum_zeroise(uint64_t *const array, size_t nb_64b_words);
+CRYPTO_FUNCTION void soft_bignum_zeroise(uint64_t *const array,
+                                         size_t nb_64b_words);
 
 /**
  * @brief Do big number ber addition
@@ -79,11 +80,30 @@ CRYPTO_FUNCTION void soft_bignum_zeroise(uint64_t *const array, size_t nb_64b_wo
  * @param nb_32b_words      number of 32 bits words to use in calcul
  * @return  the carry from the addition
  * @warning Warning the big number need to be little endian convert if necessary
- * @warning nb_32b_words is limited to 
+ * @warning nb_32b_words is limited to 0x3FFFFFFF
  */
 CRYPTO_FUNCTION uint64_t soft_bignum_add(const uint64_t *const in_a,
-                                                const uint64_t *const in_b,
-                                                uint64_t *const out,
-                                                size_t nb_32b_words);
+                                         const uint64_t *const in_b,
+                                         uint64_t *const out,
+                                         size_t nb_32b_words);
+
+/**
+ * @brief Do big number ber substraction
+ *
+ * @param in_a              Input array a
+ * @param in_b              Input array b
+ * @param out               Output array (substration result)
+ * @param nb_32b_words      number of 32 bits words to use in calcul
+ * @return  the carry from the substraction
+ * @warning Warning the big number need to be little endian convert if necessary
+ * @warning nb_32b_words is limited to 0x3FFFFFFF
+ * @warning bignumber in input are considered unsigned
+ * @warning carry is set when in_a < in_b (in case a positive number is
+ * intended, you can do a bitwise not)
+ */
+CRYPTO_FUNCTION uint32_t soft_bignum_sub(const uint64_t *const in_a,
+                                         const uint64_t *const in_b,
+                                         uint64_t *const out,
+                                         size_t nb_32b_words);
 
 #endif /* _SOFT_BIGNUMBERS_H */
--- src/api/software/bignumbers/soft_bignumbers.c
+++ src/api/software/bignumbers/soft_bignumbers.c
@@ -46,9 +46,10 @@ void soft_bignum_zeroise(uint64_t *const array, size_t nb_64b_words)
     }
 }
 
-__attribute__((naked)) uint64_t
-soft_bignum_add(const uint64_t *const in_a, const uint64_t *const in_b,
-                       uint64_t *const out, size_t nb_32b_words)
+__attribute__((naked)) uint64_t soft_bignum_add(const uint64_t *const in_a,
+                                                const uint64_t *const in_b,
+                                                uint64_t *const out,
+                                                size_t nb_32b_words)
 {
     /* just to clear warnings */
     (void)in_a;
@@ -183,6 +184,157 @@ soft_bignum_add(const uint64_t *const in_a, const uint64_t *const in_b,
 #error Please reevalute this algorithm to check validity with this xlen
 #endif
 
-    /* just for warning */
+    /* just to clear warnings */
+    return (0);
+}
+
+// w = in_a - in_b, in_a > in_b
+__attribute__((naked)) uint32_t soft_bignum_sub(const uint64_t *const in_a,
+                                                const uint64_t *const in_b,
+                                                uint64_t *const out,
+                                                size_t nb_32b_words)
+{
+    /* just to clear warnings */
+    (void)in_a;
+    (void)in_b;
+    (void)out;
+    (void)nb_32b_words;
+
+    /**
+     * just as a reminder:
+     * in_a => a0 register
+     * in_b => a1 register
+     * out => a2 register
+     * nb_32b_words => a3 register
+     */
+
+#if __riscv_xlen == 32
+    __asm__ __volatile__(
+        /* init carry to 0 */
+        "mv a5, zero \n"
+
+        /* check word_size is >0 and < 0x3FFFFFFF*/
+        "li t1, 0x3FFFFFFF \n"
+        "beqz a3, 2f\n"
+        "bgtu a3, t1, 2f\n"
+
+        /* stop condition */
+        "slli a3, a3, 2 \n"
+        "add a3, a0, a3 \n"
+
+        /* start loop */
+        "1: \n"
+        /*  load values */
+        "   lw t1, 0(a0) \n"
+        "   lw t2, 0(a1) \n"
+
+        /*  sub elements */
+        /*  add carry to t2*/
+        "   sub a5, t1, a5 \n"
+        "   sltu a4, t1, a5 \n"
+        /*  in_a[i] - in_b[i] */
+        "   sub t0, a5, t2 \n"
+        /*  if res >  in_a[i] */
+        "   sltu a5, a5, t0 \n"
+        "   or a5, a5, a4 \n"
+
+        /*  store 32 bits result */
+        "   sw t0, 0(a2) \n"
+
+        /*  update pointer addr */
+        "   addi a0, a0, 4 \n"
+        "   addi a1, a1, 4 \n"
+        "   addi a2, a2, 4 \n"
+
+        "   bltu a0, a3, 1b \n"
+
+        /* return carry (a5) */
+        "2: \n"
+        "   mv a0, a5 \n"
+        "   mv a1, zero \n"
+        "   ret \n" ::
+            :);
+#elif __riscv_xlen == 64
+    __asm__ __volatile__(
+        /* init carry to 0 */
+        "mv a5, zero \n"
+
+        /* check word_size is >0 and < 0x3FFFFFFF*/
+        "li t1, 0x3FFFFFFF \n"
+        "beqz a3, 3f\n"
+        "bgtu a3, t1, 3f\n"
+
+        /* stop condition */
+        "slli a3, a3, 2 \n"
+        "add a3, a0, a3 \n"
+
+        /* case there is only one 32bit word */
+        "addi a4, a0, 4 \n"
+        "beq a3, a4, 2f\n"
+
+        /* stop condition again (in case of 32 not odd) */
+        "mv a4, a3 \n"
+        "srl a3, a3, 3 \n"
+        "sll a3, a3, 3 \n"
+
+        /* start loop */
+        "1: \n"
+        /*  load values */
+        "   ld t1, 0(a0) \n"
+        "   ld t2, 0(a1) \n"
+
+        /*  sub elements */
+        /*  sub carry */
+        "   sub a5, t1, a5 \n"
+        /*  if res >  in_a[i] */
+        "   sltu a6, t1, a5 \n"
+        /*  in_a[i] + in_b[i] */
+        "   sub t0, a5, t2 \n"
+        /*  if res >  in_a[i] */
+        "   sltu a5, a5, t0 \n"
+        "   or a5, a5, a6 \n"
+
+        /*  store 32 bits result */
+        "   sd t0, 0(a2) \n"
+
+        /*  update pointer addr */
+        "   addi a0, a0, 8 \n"
+        "   addi a1, a1, 8 \n"
+        "   addi a2, a2, 8 \n"
+
+        "   bltu a0, a3, 1b \n"
+
+        /*  in case the last block was 64 bits return */
+        "   beq a4, a3, 3f \n"
+
+        "2: \n"
+        /*  load values */
+        "   lwu t1, 0(a0) \n"
+        "   lwu t2, 0(a1) \n"
+
+        /*  sub elements */
+        /*  sub carry */
+        "   sub a5, t1, a5 \n"
+        /*  if res >  in_a[i] */
+        "   sltu a6, t1, a5 \n"
+        /*  in_a[i] + in_b[i] */
+        "   sub t0, a5, t2 \n"
+        /*  if res >  in_a[i] */
+        "   sltu a5, a5, t0 \n"
+        "   or a5, a5, a6 \n"
+
+        /* store 32 bits result */
+        "   sw t0, 0(a2) \n"
+
+        /* return carry (a5) */
+        "3: \n"
+        "   mv a0, a5 \n"
+        "   ret \n" ::
+            :);
+#else
+#error Please reevalute this algorithm to check validity with this xlen
+#endif
+
+    /* just to clear warnings */
     return (0);
 }
